3211af2f4e351b627f30ae3ace766df1dd75eb28
Gerard Roche <gerardroche@users.noreply.github.com>
2017-08-30 14:20:35 +0100

    Enhancement: Improve code coverage report

--- plugin.py
+++ plugin.py
@@ -236,6 +236,94 @@ class CodeCoverage():
         self.output.write('\n')
         self.output.write('Generating code coverage report ...\n\n')
 
+        default_syntaxes = [
+            'Packages/R/Rd (R Documentation).sublime-syntax',
+            'Packages/R/R Console.sublime-syntax',
+            'Packages/R/R.sublime-syntax',
+            'Packages/ShellScript/Shell-Unix-Generic.sublime-syntax',
+            'Packages/Groovy/Groovy.sublime-syntax',
+            'Packages/Scala/Scala.sublime-syntax',
+            'Packages/Ruby/Ruby.sublime-syntax',
+            'Packages/Haskell/Haskell.sublime-syntax',
+            'Packages/Haskell/Literate Haskell.sublime-syntax',
+            'Packages/Textile/Textile.sublime-syntax',
+            'Packages/Makefile/Make Output.sublime-syntax',
+            'Packages/Makefile/Makefile.sublime-syntax',
+            'Packages/C++/C.sublime-syntax',
+            'Packages/C++/C++.sublime-syntax',
+            'Packages/HTML/HTML.sublime-syntax',
+            'Packages/Rails/HTML (Rails).sublime-syntax',
+            'Packages/Rails/SQL (Rails).sublime-syntax',
+            'Packages/Rails/Ruby on Rails.sublime-syntax',
+            'Packages/Rails/Ruby Haml.sublime-syntax',
+            'Packages/Rails/JavaScript (Rails).sublime-syntax',
+            'Packages/Objective-C/Objective-C++.sublime-syntax',
+            'Packages/Objective-C/Objective-C.sublime-syntax',
+            'Packages/PHP/PHP.sublime-syntax',
+            'Packages/PHP/PHP Source.sublime-syntax',
+            'Packages/Markdown/MultiMarkdown.sublime-syntax',
+            'Packages/Markdown/Markdown.sublime-syntax',
+            'Packages/Graphviz/DOT.sublime-syntax',
+            'Packages/ASP/HTML-ASP.sublime-syntax',
+            'Packages/ASP/ASP.sublime-syntax',
+            'Packages/LaTeX/LaTeX Log.sublime-syntax',
+            'Packages/LaTeX/Bibtex.sublime-syntax',
+            'Packages/LaTeX/LaTeX.sublime-syntax',
+            'Packages/LaTeX/TeX.sublime-syntax',
+            'Packages/JavaScript/Regular Expressions (JavaScript).sublime-syntax',
+            'Packages/JavaScript/JSON.sublime-syntax',
+            'Packages/JavaScript/JavaScript.sublime-syntax',
+            'Packages/CSS/CSS.sublime-syntax',
+            'Packages/Matlab/Matlab.sublime-syntax',
+            'Packages/Rust/Cargo.sublime-syntax',
+            'Packages/Rust/Rust.sublime-syntax',
+            'Packages/Regular Expressions/RegExp.sublime-syntax',
+            'Packages/XML/XML.sublime-syntax',
+            'Packages/Lua/Lua.sublime-syntax',
+            'Packages/AppleScript/AppleScript.sublime-syntax',
+            'Packages/Java/Java.sublime-syntax',
+            'Packages/Java/JavaProperties.sublime-syntax',
+            'Packages/Java/JavaDoc.sublime-syntax',
+            'Packages/Java/Java Server Pages (JSP).sublime-syntax',
+            'Packages/ActionScript/ActionScript.sublime-syntax',
+            'Packages/SQL/SQL.sublime-syntax',
+            'Packages/Python/Python.sublime-syntax',
+            'Packages/Python/Regular Expressions (Python).sublime-syntax',
+            'Packages/OCaml/camlp4.sublime-syntax',
+            'Packages/OCaml/OCamllex.sublime-syntax',
+            'Packages/OCaml/OCaml.sublime-syntax',
+            'Packages/OCaml/OCamlyacc.sublime-syntax',
+            'Packages/Erlang/HTML (Erlang).sublime-syntax',
+            'Packages/Erlang/Erlang.sublime-syntax',
+            'Packages/Diff/Diff.sublime-syntax',
+            'Packages/Go/Go.sublime-syntax',
+            'Packages/Pascal/Pascal.sublime-syntax',
+            'Packages/C#/Build.sublime-syntax',
+            'Packages/C#/C#.sublime-syntax',
+            'Packages/Perl/Perl.sublime-syntax',
+            'Packages/D/D.sublime-syntax',
+            'Packages/RestructuredText/reStructuredText.sublime-syntax',
+            'Packages/TCL/HTML (Tcl).sublime-syntax',
+            'Packages/TCL/Tcl.sublime-syntax',
+            'Packages/YAML/YAML.sublime-syntax',
+            'Packages/Batch File/Batch File.sublime-syntax',
+            'Packages/Clojure/Clojure.sublime-syntax',
+            'Packages/Lisp/Lisp.sublime-syntax',
+        ]
+
+        minimal_syntaxes = [
+            'Packages/Ruby/Ruby.sublime-syntax',
+            'Packages/C++/C.sublime-syntax',
+            'Packages/HTML/HTML.sublime-syntax',
+            'Packages/PHP/PHP.sublime-syntax',
+            'Packages/Markdown/Markdown.sublime-syntax',
+            'Packages/JavaScript/JSON.sublime-syntax',
+            'Packages/JavaScript/JavaScript.sublime-syntax',
+            'Packages/CSS/CSS.sublime-syntax',
+            'Packages/XML/XML.sublime-syntax',
+            'Packages/Python/Python.sublime-syntax',
+        ]
+
         minimal_scopes = [
             'comment',
             'constant',
@@ -290,52 +378,66 @@ class CodeCoverage():
                         colors.add(struct['settings']['background'].lower())
 
                     if 'fontStyle' in struct['settings']:
-                        styles.add(struct['settings']['fontStyle'].lower())
+                        if struct['settings']['fontStyle']:
+                            styles.add(struct['settings']['fontStyle'].lower())
 
             report_data.append({
                 'color_scheme': color_scheme,
                 'syntaxes': syntaxes,
+                'default_syntaxes': syntaxes & set(default_syntaxes),
+                'minimal_syntaxes': syntaxes & set(minimal_syntaxes),
                 'colors': colors,
                 'scopes': scopes,
+                'minimal_scopes': scopes & set(minimal_scopes),
                 'styles': styles
             })
 
-        self.output.write('{: <50} {:12} {:6} {:6}\n'.format('Name', 'Syntax tests', 'Colors', 'Scopes'))
-        self.output.write('-----------------------------------------------------------------------------\n')
+        cs_col_w = max([len(x['color_scheme']) for x in report_data])
+        template = '{: <' + str(cs_col_w) + '} {: >12} {: >8} {: >14} {: >7}\n'
 
-        for info in report_data:
-            self.output.write('{: <50} {:12} {:6} {:6}\n'.format(
+        self.output.write(template.format('Name', '', 'Syntaxes', 'Scopes', 'Colors'))
+        self.output.write(('-' * cs_col_w) + '---------------------------------------------\n')
+        for info in sorted(report_data, key=lambda x: x['color_scheme']):
+            self.output.write(template.format(
                 info['color_scheme'],
-                len(info['syntaxes']),
+                '{} ({}/{})'.format(len(info['syntaxes']), len(info['minimal_syntaxes']), len(minimal_syntaxes)),
+                '({}/{})'.format(len(info['default_syntaxes']), len(default_syntaxes)),
+                '{} ({}/{})'.format(len(info['scopes']), len(info['minimal_scopes']), len(minimal_scopes)),
                 len(info['colors']),
-                len(info['scopes'])
             ))
 
         self.output.write('\n')
 
-        for i, info in enumerate(report_data, start=1):
-            self.output.write('{}) {}\n'.format(i, info['color_scheme']))
+        for i, info in enumerate(sorted(report_data, key=lambda x: x['color_scheme']), start=1):
+            self.output.write('{}) {}\n\n'.format(i, info['color_scheme']))
+            self.output.write('   Colors ({}) {}\n'.format(len(info['colors']), sorted(info['colors'])))
+            self.output.write('   Styles ({}) {}\n'.format(len(info['styles']), sorted(info['styles'])))
+            self.output.write('   Syntaxes ({}) {}\n'.format(len(info['syntaxes']), sorted(info['syntaxes'])))
+            self.output.write('   Scopes ({}) {}\n'.format(len(info['scopes']), sorted(info['scopes'])))
 
-            self.output.write('  Colors ({})\n'.format(len(info['colors'])))
-            for color in sorted(info['colors']):
-                self.output.write('    {}\n'.format(color))
+            syntaxes_not_covered = [s for s in sorted(minimal_syntaxes) if s not in info['syntaxes']]
+            scopes_not_covered = [s for s in sorted(minimal_scopes) if s not in info['scopes']]
+            total_notice_count = len(syntaxes_not_covered) + len(scopes_not_covered)
 
-            self.output.write('  Syntaxes tested ({})\n'.format(len(info['syntaxes'])))
-            for syntax in sorted(info['syntaxes']):
-                self.output.write('    {}\n'.format(syntax))
+            if total_notice_count:
+                self.output.write('\n')
+                self.output.write('   There %s %s notice%s:\n' % (
+                    'is' if total_notice_count == 1 else 'are',
+                    total_notice_count,
+                    '' if total_notice_count == 1 else 's',
+                ))
 
-            self.output.write('  Minimal scope coverage\n')
-            for scope in sorted(minimal_scopes):
-                has_scope = True if scope in info['scopes'] else False
-                self.output.write('    {:<1} {}\n'.format(has_scope, scope))
+                if syntaxes_not_covered:
+                    self.output.write('\n')
+                    self.output.write('   Minimal syntaxes not covered ({}):\n\n'.format(len(syntaxes_not_covered)))
+                    for i, syntax in enumerate(syntaxes_not_covered, start=1):
+                        self.output.write('   * {}\n'.format(syntax))
 
-            # self.output.write('  Styles ({})\n'.format(len(info['styles'])))
-            # for style in sorted(info['styles']):
-            #     self.output.write('    {}\n'.format(style))
-
-            # self.output.write('  Scopes ({})\n'.format(len(info['scopes'])))
-            # for scope in sorted(info['scopes']):
-            #     self.output.write('    {}\n'.format(scope))
+                if scopes_not_covered:
+                    self.output.write('\n')
+                    self.output.write('   Minimal scopes not covered ({}):\n\n'.format(len(scopes_not_covered)))
+                    for i, scope in enumerate(scopes_not_covered, start=1):
+                        self.output.write('   * {}\n'.format(scope))
 
             self.output.write('\n')
 
