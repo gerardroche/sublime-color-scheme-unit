e682e915ef65deac4efa2fc5da12b9ed770ed524
Gerard Roche <gerardroche@users.noreply.github.com>
2023-06-04 01:18:52 +0100

    Bump version

--- lib/runner.py
+++ lib/runner.py
@@ -17,9 +17,9 @@ from ColorSchemeUnit.lib.test import TestOutputPanel
 from ColorSchemeUnit.lib.test import TestView
 
 
-__version__ = "2.0.0"
+__version__ = "2.2.0"
 
-__version_info__ = (2, 0, 0)
+__version_info__ = (2, 2, 0)
 
 _color_test_params_compiled_pattern = re.compile(
     '^(?:(?:\\<\\?php )?(?://|#|\\/\\*|\\<\\!--|--)\\s*)?'
